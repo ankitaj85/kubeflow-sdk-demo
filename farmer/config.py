138a51a609bc776a0984b2977d3152b42efd2b66
Yogender singh <syogender799@gmail.com>
2021-03-08 19:40:09 +0530

    changes in notebook

--- farmer/config.py
+++
@@ -1,6 +0,0 @@
-gs_bucket_name="data-labeling-demo"
-Bucket_uri="gs://data-labeling-demo"
-version=1
-store_artifacts=Bucket_uri + "/" + str(version)
-data_path=Bucket_uri + "/" + "data/data_raw.csv"
-processed_data=Bucket_uri + "/" + "processed/data_processed.csv"
